c295cf8ea9e89daa102d15f1f4889a144035395b
Dan Soppelsa <dsoppelsa@gmail.com>
2017-09-24 16:05:12 -0400

    Update Command registration test to handle all carwash comamnds

--- tests/CommandRegistrationTest.php
+++ tests/CommandRegistrationTest.php
@@ -1,26 +1,20 @@
 <?php namespace Carwash;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Symfony\Component\Console\Exception\CommandNotFoundException;
 
 class CommandRegistrationTest extends TestCase
 {
     use DatabaseTransactions;
+    const CARWASH_COMMANDS = [
+        'carwash:scrub'
+    ];
 
-    public function setUp()
+    public function testThatAllComandsGetRegisteredWithTheApplication()
     {
-        parent::setUp();
-    }
+        $registeredCommands = collect(\Artisan::all())->keys();
 
-    public function testThatScrubCommandGetsRegisteredWithTheApplication()
-    {
-        try {
-            $this->artisan('carwash:scrub');
-            $registered = true;
-        } catch (CommandNotFoundException $e) {
-            $registered = false;
-        }
-
-        $this->assertTrue($registered);
+        collect(static::CARWASH_COMMANDS)->each(function ($carwashCommand) use ($registeredCommands) {
+            $this->assertTrue($registeredCommands->contains($carwashCommand));
+        });
     }
 }
